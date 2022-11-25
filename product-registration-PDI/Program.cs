b519ba2646f1b3da48bc74bc7a29c2ed932cf9e0
Joao Vasconcelos <joao.vasconcelos@take.net>
2022-11-24 22:43:18 -0300

    Add project files.

---
+++ product-registration-PDI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace product_registration_PDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
